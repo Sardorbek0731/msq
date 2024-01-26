9ecc5523adae95fdb2d752463febd448f7c2094b
Sardorbek Olimov <sardor.20070731@gmail.com>
2024-01-26 23:01:46 +0500

    repaired header

--- src/components/navbar/Header.tsx
+++ src/components/navbar/Header.tsx
@@ -1,7 +1,42 @@
+// CSS
 import "./Header.css";
 
+// Images
+import logo from "../../assets/images/logo.png";
+
 function Header() {
-  return <div>Header</div>;
+  //   let navbarLists = ["Loyihalar", "Biz haqimizda", "Kontakt"];
+
+  return (
+    <header>
+      <div className="container">
+        <nav className="nav flex alignCenter justifyBetween">
+          <div className="logo flex alignCenter">
+            <img src={logo} alt="Logo" />
+            <h3>Muhammad Savdo Qurilish MCHJ</h3>
+          </div>
+
+          <ul className="flex alignCenter">
+            <li>
+              <a href="#">Loyihalar</a>
+            </li>
+
+            <li>
+              <a href="#">Biz haqimizda</a>
+            </li>
+
+            <li>
+              <a href="#">Kontakt</a>
+            </li>
+          </ul>
+
+          <a className="number" href="tel: +998941173949">
+            94 117 3949
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
