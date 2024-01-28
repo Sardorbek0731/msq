c6a52ef4c5c9c2be9801dceb555a8f6eacad962f
Sardorbek Olimov <sardor.20070731@gmail.com>
2024-01-28 19:38:33 +0500

    repaired about us section

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,4 +1,8 @@
+// CSS
 import "./About.css";
+
+// Images
+import aboutImg from "../../assets/images/about.jpg";
 
 function About({ setClickLang }) {
   return (
@@ -15,6 +19,34 @@ function About({ setClickLang }) {
             <span className="firstLetter">B</span>
             iz haqimizda ...
           </h1>
+        </div>
+        <div className="aboutInfos flex">
+          <div className="aboutUs_text flex column">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+              possimus dolor pariatur facilis culpa voluptates eaque eveniet
+              voluptatum. possimus dolor pariatur facilis culpa voluptates eaque
+              eveniet voluptatum.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+              possimus dolor pariatur facilis culpa voluptates eaque eveniet
+              voluptatum, qui officia inventore vel earum expedita recusandae,
+              impedit quidem dolorem? Commodi fugit blanditiis
+              facilisvoluptatum, qui officia inventore vel earum expedita
+              recusandae, impedit quidem dolorem? Commodi fugit blanditiis
+              facilisvoluptatum, qui officia inventore vel earum expedita
+              recusandae, impedit quidem dolorem? Commodi fugit blanditiis
+              facilis
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+              possimus dolor pariatur facilis culpa voluptates.
+            </p>
+          </div>
+          <div className="aboutUs_img flex">
+            <img src={aboutImg} alt="About us image" />
+          </div>
         </div>
       </div>
     </section>
