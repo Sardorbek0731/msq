200c54e6dcb50be46aa0b66bc0eefff9aa34f2ed
Sardorbek Olimov <sardor.20070731@gmail.com>
2024-02-02 23:27:12 +0500

    changing advantage section design

--- src/components/advantages/Advantages.tsx
+++ src/components/advantages/Advantages.tsx
@@ -1,31 +1,34 @@
 import "./Advantages.css";
-import { AttachMoney, ThumbUpAlt, Timer } from "@mui/icons-material";
 import { useTranslation } from "react-i18next";
+
+// Icons
+import like from "../../assets/icons/like.png";
+import speed from "../../assets/icons/speed.png";
+import coin from "../../assets/icons/coin.png";
 
 const Advantages = () => {
   const { t } = useTranslation();
 
   const advantage = [
     {
-      icon: <ThumbUpAlt className="Icon" />,
+      icon: like,
       title: "Yuqori sifat",
       description: `Lorem ipsum dolor amet consectetur adipiscing dolore magna aliqua
 enim`,
     },
     {
-      icon: <AttachMoney className="Icon" />,
+      icon: speed,
       title: "Qulay narx",
       description: `Lorem ipsum dolor amet consectetur adipiscing dolore magna aliqua
 enim`,
     },
     {
-      icon: <Timer className="Icon" />,
+      icon: coin,
       title: "Yuqori tezlik",
       description: `Lorem ipsum dolor amet consectetur adipiscing dolore magna aliqua
 enim`,
     },
   ];
-
   return (
     <section className="advantages">
       <div className="container">
@@ -35,7 +38,7 @@ enim`,
         <div className="OurAdvantagesRow">
           {advantage.map((adv, index) => (
             <div className="IconBox" key={index}>
-              <div className="IconWrap">{adv.icon}</div>
+              <img src={adv.icon} alt="Logo" />
               <h4 className="title">{adv.title}</h4>
               <p className="description">{adv.description}</p>
             </div>
