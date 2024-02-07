38c4ae0b87adcc6fae7f051ab977665234904bbd
Sardorbek Olimov <sardor.20070731@gmail.com>
2024-02-07 21:54:57 +0500

    changed navbar design

--- src/components/navbar/Header.tsx
+++ src/components/navbar/Header.tsx
@@ -7,7 +7,8 @@ import logo from "../../assets/images/logo.png";
 // Icons
 import uz from "../../assets/icons/uz.png";
 import ru from "../../assets/icons/ru.png";
-import navBtn from "../../assets/icons/column-menu.png";
+import navOpen from "../../assets/icons/open.png";
+import navClose from "../../assets/icons/close.png";
 
 // Hooks
 import { useState } from "react";
@@ -15,6 +16,7 @@ import { useTranslation } from "react-i18next";
 
 function Header({ clickLang, setClickLang }) {
   const [langValue, setLangValue] = useState<string>("O'zbek");
+  const [openNav, setOpenNav] = useState<boolean>(false);
   const { t, i18n } = useTranslation();
 
   interface INavbar {
@@ -45,12 +47,21 @@ function Header({ clickLang, setClickLang }) {
     <header className="flex alignCenter wf-100">
       <div className="container wf-100">
         <nav className="nav flex alignCenter justifyBetween">
-          <a href="#" className="logo flex alignCenter">
+          <a
+            href="#"
+            className={
+              openNav ? "logo flex alignCenter fixed" : "logo flex alignCenter"
+            }
+          >
             <img src={logo} alt="Logo" />
             <h3>Muhammad Savdo Qurilish MCHJ</h3>
           </a>
 
-          <ul className="flex alignCenter">
+          <ul
+            className={
+              openNav ? "flex alignCenter activeNav" : "flex alignCenter"
+            }
+          >
             {navbarLists.map((item: INavbar, index: number) => {
               return (
                 <li key={index}>
@@ -62,69 +73,85 @@ function Header({ clickLang, setClickLang }) {
 
           <div
             className={
-              clickLang
-                ? "language_btn flex alignCenter clickedLangBtn"
-                : "language_btn flex alignCenter"
+              openNav
+                ? "langAnd_navBtn flex alignCenter justifyBetween fixed"
+                : "langAnd_navBtn flex alignCenter justifyBetween"
             }
-            onClick={() => {
-              setClickLang(!clickLang);
-            }}
           >
-            <img src={langValue == "O'zbek" ? uz : ru} alt="Flag icon" />
-            <h1>{langValue}</h1>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-chevron-down"
-              viewBox="0 0 16 16"
-              id={clickLang ? "lang-arrow" : ""}
-            >
-              <path
-                fillRule="evenodd"
-                d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
-              ></path>
-            </svg>
-
             <div
               className={
-                clickLang ? "language_items clickLang" : "language_items"
+                clickLang
+                  ? "language_btn flex alignCenter clickedLangBtn"
+                  : "language_btn flex alignCenter"
               }
+              onClick={() => {
+                setClickLang(!clickLang);
+              }}
             >
+              <img src={langValue == "O'zbek" ? uz : ru} alt="Flag icon" />
+              <h1>{langValue}</h1>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-chevron-down"
+                viewBox="0 0 16 16"
+                id={clickLang ? "lang-arrow" : ""}
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                ></path>
+              </svg>
+
               <div
                 className={
-                  langValue == "O'zbek"
-                    ? "language_item activeLang"
-                    : "language_item"
+                  clickLang ? "language_items clickLang" : "language_items"
                 }
-                onClick={() => {
-                  setLangValue("O'zbek");
-                  i18n.changeLanguage("uz");
-                }}
               >
-                <img src={uz} alt="Flag icon" />
-                O'zbek
-              </div>
-              <div
-                className={
-                  langValue == "Русский"
-                    ? "language_item activeLang"
-                    : "language_item"
-                }
-                onClick={() => {
-                  setLangValue("Русский");
-                  i18n.changeLanguage("ru");
-                }}
-              >
-                <img src={ru} alt="Flag icon" />
-                Русский
+                <div
+                  className={
+                    langValue == "O'zbek"
+                      ? "language_item activeLang"
+                      : "language_item"
+                  }
+                  onClick={() => {
+                    setLangValue("O'zbek");
+                    i18n.changeLanguage("uz");
+                  }}
+                >
+                  <img src={uz} alt="Flag icon" />
+                  O'zbek
+                </div>
+                <div
+                  className={
+                    langValue == "Русский"
+                      ? "language_item activeLang"
+                      : "language_item"
+                  }
+                  onClick={() => {
+                    setLangValue("Русский");
+                    i18n.changeLanguage("ru");
+                  }}
+                >
+                  <img src={ru} alt="Flag icon" />
+                  Русский
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="columnNavbar_btn">
-            <img src={navBtn} alt="Column Menu Button" />
+            <div
+              className="columnNavbar_btn hidden"
+              onClick={() => {
+                setOpenNav(!openNav);
+              }}
+            >
+              <img
+                src={openNav ? navClose : navOpen}
+                alt="Column Menu Button"
+              />
+            </div>
           </div>
         </nav>
       </div>
