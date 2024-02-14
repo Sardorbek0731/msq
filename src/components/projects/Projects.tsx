f5e70ff06e0a1cfa82828a71020a31cf174322dd
Sardorbek Olimov <sardor.20070731@gmail.com>
2024-02-14 21:01:44 +0500

    repairing fullscreen project images

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -9,6 +9,10 @@ import project4 from "../../assets/images/project-4.jpg";
 import project5 from "../../assets/images/project-5.jpg";
 import project6 from "../../assets/images/project-6.jpg";
 import project7 from "../../assets/images/project-7.jpg";
+import fullscreen from "../../assets/icons/fullscreen.png";
+import close from "../../assets/icons/close-white.png";
+import left from "../../assets/icons/left.png";
+import right from "../../assets/icons/right.png";
 import { useTranslation } from "react-i18next";
 
 function Projects({ setClickLang }) {
@@ -58,10 +62,14 @@ function Projects({ setClickLang }) {
         <div className="sectionTitle">
           <h1>{t("navbar.projects") + "..."}</h1>
         </div>
+
         <div className="rowMain flex justifyBetween">
           <div className="firstProject">
             <img src={project1} alt="Project Images" />
             <h2>Al-Xorazmiy majmuasidagi bolalar maydonchasi</h2>
+            <div className="fullscreenBtn">
+              <img src={fullscreen} alt="FullScreen Button" />
+            </div>
           </div>
 
           <div className="columnImgs flex">
@@ -70,9 +78,41 @@ function Projects({ setClickLang }) {
                 <div className="projectImg" key={index}>
                   <img src={item.image} alt="Project Images" />
                   <h2>{item.title}</h2>
+
+                  <div className="fullscreenBtn">
+                    <img src={fullscreen} alt="FullScreen Button" />
+                  </div>
                 </div>
               );
             })}
+          </div>
+        </div>
+
+        <div className="fullscreenImages flex justifyBetween">
+          <div className="fullscreenImage flex alignCenter justifyCenter">
+            <img src={project1} alt="Project Images" />
+          </div>
+          {projects.map((item: IProject, index: number) => {
+            return (
+              <div
+                className="fullscreenImage flex alignCenter justifyCenter"
+                key={index}
+              >
+                <img src={item.image} alt="Project Images" />
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="fullscreenButtons">
+          <div className="closeFullscreen flex alignCenter justifyCenter">
+            <img src={close} alt="Close Fullscreen" />
+          </div>
+          <div className="leftBtn sideBtn flex alignCenter justifyCenter">
+            <img src={left} alt="Left Fullscreen" />
+          </div>
+          <div className="rightBtn sideBtn flex alignCenter justifyCenter">
+            <img src={right} alt="Right Fullscreen" />
           </div>
         </div>
       </div>
